43d8700398ef69d34f1d98420b5a3ec1689cdb92
Peet Liebenberg <jliebenberg@my.bcit.ca>
2019-01-17 09:40:48 -0800

    Add project files.

---
+++ WebAPIIC3/Controllers/ToDosController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPIIC3.DatabaseHelper;
+
+namespace WebAPIIC3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDosController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public ToDosController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ToDos
+        [HttpGet]
+        public IEnumerable<ToDo> GetToDos()
+        {
+            return _context.ToDos;
+        }
+
+        // GET: api/ToDos/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetToDo([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var toDo = await _context.ToDos.FindAsync(id);
+
+            if (toDo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(toDo);
+        }
+
+        // PUT: api/ToDos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutToDo([FromRoute] int id, [FromBody] ToDo toDo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != toDo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(toDo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ToDoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ToDos
+        [HttpPost]
+        public async Task<IActionResult> PostToDo([FromBody] ToDo toDo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.ToDos.Add(toDo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetToDo", new { id = toDo.Id }, toDo);
+        }
+
+        // DELETE: api/ToDos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDo([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var toDo = await _context.ToDos.FindAsync(id);
+            if (toDo == null)
+            {
+                return NotFound();
+            }
+
+            _context.ToDos.Remove(toDo);
+            await _context.SaveChangesAsync();
+
+            return Ok(toDo);
+        }
+
+        private bool ToDoExists(int id)
+        {
+            return _context.ToDos.Any(e => e.Id == id);
+        }
+    }
+}
